2c8d809f16b2d51c7c27a563b33ce30817fa8794
Jose Luis <joseluis27@ciencias.unam.mx>
2023-04-13 18:47:10 -0600

    Lectura de texto y generacion de arreglo

---
+++ src/Lector.java
@@ -0,0 +1,61 @@
+
+import java.io.*;
+import java.util.*;
+/**
+ * Lector
+ */
+public class Lector {
+    
+    private String texto;
+    private String[] arregloPalabras;
+    
+    public Lector(String nombreArchivo) {
+        this.arregloPalabras = lectura(nombreArchivo);        
+    }
+
+    public String[] lectura(String nombreArchivo){ 
+        String cadena = "";
+        String palabra = "";
+        List<String> palabras = new ArrayList<String>();
+        try {
+            FileReader archivo = new FileReader(nombreArchivo);
+
+            int i;
+
+            while ((i = archivo.read()) != -1 ) {
+                if ((i == 32) || (i == 46) ||(i == 44)) {
+                    palabras.add(palabra);
+                    palabra = "";
+                }
+                palabra += (char)i;
+                
+                cadena += (char)i; 
+            }    
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        List<String> punto = new ArrayList<String>();
+        punto.add(".");
+        punto.add(",");
+        palabras.removeAll(punto);
+
+        String [] palabrasArr = new String[palabras.size()];
+        palabrasArr = palabras.toArray(palabrasArr);
+
+        this.setTexto(cadena);
+        return palabrasArr;
+    }
+
+    public String[] getPalabras() {
+        return this.arregloPalabras;
+    }
+
+    public String getTexto() {
+        return this.texto;
+    } 
+    
+    public void setTexto(String texto){
+        this.texto = texto;
+    }
+}
---
+++ src/Prueba.java
@@ -0,0 +1,28 @@
+
+public class Prueba {
+
+    public static void main(String[] args) {
+
+        Lector lectorTexto = new Lector("../LoR.txt");
+    
+        //System.out.println(((Object)texto).getClass().getSimpleName()); Conocer el tipo de una variable
+        // boolean procesador1;
+        // boolean procesador2;
+        // boolean procesador3;
+        // String textoCompleto;
+
+        // textoCompleto = " Uno para el Señor Oscuro, sobre el trono obscuro en la Tierra de Mordor donde se extienden las Sombras. ";
+
+        // Procesador p =  new Procesador();
+
+        // procesador1 = p.procesado1(textoCompleto);
+        // procesador2 = p.procesado2("");
+        // procesador3 = p.procesado3(" Uno para el Señor Oscuro, sobre el trono obscuro en la Tierra de Mordor donde se extienden las Sombras. ");
+
+        // System.out.println("Primera funcion " + procesador1);
+        // System.out.println("Segunda funcion " + procesador2);
+        // System.out.println("Tercera funcion " + procesador3);
+
+    }    
+
+}
